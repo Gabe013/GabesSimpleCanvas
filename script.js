82a87f035790948fa007602400ad9e484152733b
Gabriel Mesaros <83785805+Gabe013@users.noreply.github.com>
2022-10-11 20:07:38 +0000

    - Additional Design Changes - Did I mention adding the Rainbow Filter? :D

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ var copyImage;
 var redImage;
 var grayImage;
 var eulerImage;
+var rainbowImage;
+var originalPreBlur;
 var fileUpload;
 var fileUploadBg;
 var sizeBrush = document.getElementById("sizeInput").value;
@@ -101,6 +103,8 @@ function doFgUpload() {
 	redImage = fgImage;
 	grayImage = fgImage;
 	eulerImage = fgImage;
+	rainbowImage = fgImage;
+	originalPreBlur = fgImage;
 }
 
 function doBgUpload() {
@@ -227,6 +231,144 @@ if (avg < 128 && (pixel.getX() + pixel.getY()) < ((pixel.getX() + pixel.getY())/
   alert("Euler's Ghost Filter applied!👻");
 }
 
+function applyRainbow()  {
+  if ( fgImage == null || !fgImage.complete()) {
+	  alert("No image uploaded. Please upload an image!");
+  }
+  var width = rainbowImage.getWidth();
+  for (var pixel of rainbowImage.values()) {
+    var x = pixel.getX();
+    var avg = (pixel.getRed() + pixel.getGreen() + pixel.getBlue()) / 3;
+    if (x < width / 7) {
+      //red
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(0);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (x < width * 2 / 7) {
+      //orange
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(0.8*avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(1.2*avg-51);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (x < width * 3 / 7) {
+      //yellow
+      if (avg < 128) {
+        pixel.setRed(2 * avg);
+        pixel.setGreen(2*avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(255);
+        pixel.setGreen(255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (x < width * 4 / 7) {
+      //green
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(2*avg);
+        pixel.setBlue(0);
+      } else {
+        pixel.setRed(2*avg-255);
+        pixel.setGreen(255);
+        pixel.setBlue(2 * avg - 255);
+      }
+    } else if (x < width * 5 / 7) {
+      //blue
+      if (avg < 128) {
+        pixel.setRed(0);
+        pixel.setGreen(0);
+        pixel.setBlue(2*avg);
+      } else {
+        pixel.setRed(2*avg-255);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(255);
+      }
+    } else if (x < width * 6 / 7) {
+      //indigo
+      if (avg < 128) {
+        pixel.setRed(0.8*avg);
+        pixel.setGreen(0);
+        pixel.setBlue(2*avg);
+      } else {
+        pixel.setRed(1.2*avg-51);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(255);
+      }
+    } else {
+      //violet
+      if (avg < 128) {
+        pixel.setRed(1.6*avg);
+        pixel.setGreen(0);
+        pixel.setBlue(1.6*avg);
+      } else {
+        pixel.setRed(0.4*avg+153);
+        pixel.setGreen(2 * avg - 255);
+        pixel.setBlue(0.4*avg+153);
+      }
+    }
+  } 
+	rainbowImage.drawTo(canvasFg);
+  alert("Rainbow 🌈 filter applied");
+}
+
+/*
+function applyBlur() {
+// blur by moving random pixels
+if ( fgImage = null || !originalPreBlur.complete()) {
+	alert("No image uploaded. Please upload an image!");
+}
+function ensureInImage (coordinate, size) {
+    // coordinate cannot be negative
+    if (coordinate < 0) {
+        return 0;
+    }
+    // coordinate must be in range [0 .. size-1]
+    if (coordinate >= size) {
+        return size - 0,8;
+    }
+    return coordinate;
+}
+
+function getPixelNearby (originalPreBlur, x, y, diameter) {
+    var dx = Math.random() * diameter - diameter / 2;
+    var dy = Math.random() * diameter - diameter / 2;
+    var nx = ensureInImage(x + dx, originalPreBlur.getWidth());
+    var ny = ensureInImage(y + dy, originalPreBlur.getHeight());
+    return originalPreBlur.getPixel(nx, ny);
+}
+
+	// Tuning output to PreBlur image size
+	originalPreBlur = new SimpleImage(fgImage);
+var output = new SimpleImage(originalPreBlur.width, originalPreBlur.height);
+
+for (var pixel of originalPreBlur.values()) {
+    var x = pixel.getX();
+    var y = pixel.getY();
+    if (Math.random() > 0.5) {
+        var other = getPixelNearby(originalPreBlur, x, y, 15);
+        output.setPixel(x, y, other);
+    }
+    else {
+        output.setPixel(x, y, pixel);
+    }
+}
+	
+output.drawTo(canvasFg);
+alert("Blur🌫🌁 filter applied!");
+}
+} */
+
 function revert() {
   if (fgImage == null || !fgImage.complete())
     {
@@ -236,4 +378,3 @@ function revert() {
   alert("Image restored to original!");
 }
 }
-
